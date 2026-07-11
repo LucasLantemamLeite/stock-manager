d732c127325db8309a2f099d0efcc7316c67cdc4
Lucas Lantemam Leite <Lucaslantemamleite2005@gmail.com>
2026-07-11 14:13:24 -0300

    wip: cria a classe do AppDbContext

---
+++ StockManager.Api/Data/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockManager.Api.Data.Mappings;
+using StockManager.Api.Models;
+
+namespace StockManager.Api.Data.Context;
+
+public sealed class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; init; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UserMap());
+    }
+}
