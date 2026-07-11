d504a53269cd69f67031b0968ba4121916e72cf2
LucasLantemamLeite <lucaslantemamleite2005@gmail.com>
2026-07-11 17:36:49 -0300

    feat: arquivos de migração gerado pelo dontet-ef tool

---
+++ StockManager.Api/Migrations/20260711203059_InitialDeploy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StockManager.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDeploy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "UNIQUEIDENTIFIER", nullable: false),
+                    Name = table.Column<string>(type: "NVARCHAR(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "NVARCHAR(255)", maxLength: 255, nullable: false),
+                    Phone = table.Column<string>(type: "VARCHAR(20)", maxLength: 20, nullable: false),
+                    CompanyId = table.Column<Guid>(type: "UNIQUEIDENTIFIER", nullable: false),
+                    Role = table.Column<byte>(type: "TINYINT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "DATETIME2(0)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "DATETIME2(0)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "Unique_Key_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "Unique_Key_Users_Phone",
+                table: "Users",
+                column: "Phone",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
