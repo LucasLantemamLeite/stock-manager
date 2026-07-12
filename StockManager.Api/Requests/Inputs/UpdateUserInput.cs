18cd9a963138cec39dc6130ec39d9938b9e59f0b
LucasLantemamLeite <Lucaslantemamleite2005@gmail.com>
2026-07-12 19:49:17 -0300

    feat: cria a entrada de dados para atualizar as informações da conta do usuário na requisição

---
+++ StockManager.Api/Requests/Inputs/UpdateUserInput.cs
@@ -0,0 +1,10 @@
+﻿namespace StockManager.Api.Requests.Inputs;
+
+public sealed record UpdateUserInput(
+    string? NewName,
+    string? NewEmail,
+    string? NewPhone,
+    string? NewPassword,
+    Guid Id,
+    string ConfirmPassword
+);
