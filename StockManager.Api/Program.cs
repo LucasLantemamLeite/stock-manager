4f7f5aec967131da162f3a43170841d500462d45
LucasLantemamLeite <Lucaslantemamleite2005@gmail.com>
2026-07-12 16:48:55 -0300

    feat: resolve a injeção de dependência do AppDbContext

--- StockManager.Api/Program.cs
+++ StockManager.Api/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StockManager.Api.Data.Context;
+using StockManager.Api.Enums;
+using StockManager.Api.Models;
+using StockManager.Api.Requests.Inputs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var dbConnectionString = builder.Configuration.GetConnectionString("Default");
+
+builder.Services.AddDbContext<AppDbContext>(c => c.UseSqlServer(dbConnectionString));
 
 var app = builder.Build();
 
