0f2665062085a4973975ffc331240eb22e4dfff7
LucasLantemamLeite <Lucaslantemamleite2005@gmail.com>
2026-07-12 16:49:24 -0300

    feat: adiciona a rota temporária para criar conta do usuário

--- StockManager.Api/Program.cs
+++ StockManager.Api/Program.cs
@@ -13,4 +13,27 @@ builder.Services.AddDbContext<AppDbContext>(c => c.UseSqlServer(dbConnectionStri
 
 var app = builder.Build();
 
+app.MapPost("/v1/user", async ([FromBody] CreateUserInput requestInput, AppDbContext context) =>
+{
+    if (await context.Users.AnyAsync(u => u.Email == requestInput.Email))
+        return Results.Conflict("Email já está em uso.");
+
+    if (await context.Users.AnyAsync(u => u.Phone == requestInput.Phone))
+        return Results.Conflict("Número de telefone já está em uso.");
+
+    var userToAdd = new User(
+        name: requestInput.Name,
+        email: requestInput.Email,
+        phone: requestInput.Phone,
+        companyId: Guid.NewGuid(),
+        role: (Role)requestInput.Role
+    );
+
+    context.Users.Add(userToAdd);
+
+    await context.SaveChangesAsync();
+
+    return Results.Ok(new { message = "Usuário adicionado com sucesso.", data = userToAdd });
+});
+
 app.Run();
