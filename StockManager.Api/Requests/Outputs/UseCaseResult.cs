411a94a3707d78d133efd1dffbcd6c55ab5c3a3d
LucasLantemamLeite <Lucaslantemamleite2005@gmail.com>
2026-07-13 19:13:30 -0300

    feat: cria o record para transferir o resultado dos UseCase para o usuário na requisição

---
+++ StockManager.Api/Requests/Outputs/UseCaseResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace StockManager.Api.Requests.Outputs;
+
+public sealed record UseCaseResult<TData>(HttpStatusCode HttpStatusCode, string Message, bool StopExecution = false, TData? Data = null)
+    where TData : class
+{
+    private HttpStatusCode HttpStatusCode { get; } = HttpStatusCode;
+    public int IntStatusCode => (int)HttpStatusCode;
+    public string Message { get; } = Message;
+    public bool StopExecution { get; } = StopExecution;
+    public TData? Data { get; } = Data;
+}
