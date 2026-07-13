e55281ba773554daca6289527cedd8ae49a7f2a9
LucasLantemamLeite <Lucaslantemamleite2005@gmail.com>
2026-07-13 13:28:06 -0300

    refactor: cria um método de extensão para registrar as rotas do usuário de forma isolada do Program.cs

---
+++ StockManager.Api/Extensions/UserEndpointExtension.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StockManager.Api.Data.Context;
+using StockManager.Api.Enums;
+using StockManager.Api.Interfaces;
+using StockManager.Api.Models;
+using StockManager.Api.Requests.Inputs;
+
+namespace StockManager.Api.Extensions;
+
+public static class UserEndpointExtension
+{
+    extension(WebApplication app)
+    {
+        public void AddUserEndpoints()
+        {
+            app.MapPost("/v1/user", async ([FromBody] CreateUserInput requestInput, AppDbContext context, ITokenService tokenService, IHasherService hasherService) =>
+            {
+                if (await context.Users.AnyAsync(u => u.Email == requestInput.Email))
+                    return Results.Conflict("Email já está em uso.");
+
+                if (await context.Users.AnyAsync(u => u.Phone == requestInput.Phone))
+                    return Results.Conflict("Número de telefone já está em uso.");
+
+                var userToAdd = new User(
+                    name: requestInput.Name,
+                    email: requestInput.Email,
+                    phone: requestInput.Phone,
+                    password: hasherService.GeneratePasswordHash(requestInput.Password),
+                    companyId: Guid.NewGuid(),
+                    role: (Role)requestInput.Role
+                );
+
+                context.Users.Add(userToAdd);
+
+                await context.SaveChangesAsync();
+
+                return Results.Ok(new
+                {
+                    message = "Usuário adicionado com sucesso.",
+                    token = tokenService.GenerateAuthToken(userToAdd)
+                });
+            });
+
+            app.MapPost("/v1/user/login", async ([FromBody] LoginUserInput requestInput, AppDbContext context, ITokenService tokenService, IHasherService hasherService) =>
+            {
+                var userToLogin = await context.Users.SingleOrDefaultAsync(u => u.Email == requestInput.Email);
+
+                if (userToLogin is null || !hasherService.VerifyPasswordHash(userToLogin.Password, requestInput.ConfirmPassword))
+                    return Results.BadRequest("Credenciais incorretas.");
+
+                return Results.Ok(new { message = "Login realizado com sucesso!", token = tokenService.GenerateAuthToken(userToLogin) });
+            });
+
+            app.MapPatch("/v1/user", async ([FromBody] UpdateUserInput requestInput, AppDbContext context, IHasherService hasherService) =>
+            {
+                var userToUpdate = await context.Users.SingleOrDefaultAsync(u => u.Id == requestInput.Id);
+
+                if (userToUpdate is null || !hasherService.VerifyPasswordHash(userToUpdate.Password, requestInput.ConfirmPassword))
+                    return Results.BadRequest("Credenciais incorretas");
+
+                userToUpdate.SetName(requestInput.NewName);
+                userToUpdate.SetEmail(requestInput.NewEmail);
+                userToUpdate.SetPhone(requestInput.NewPhone);
+
+                userToUpdate.SetPassword(
+                    requestInput.NewPassword is not null
+                    ? hasherService.GeneratePasswordHash(requestInput.NewPassword)
+                    : null
+                );
+
+                userToUpdate.SetUpdateAtToNow();
+
+                await context.SaveChangesAsync();
+
+                return Results.Ok(new { message = "Usuário atualizado com sucesso!", data = userToUpdate });
+            });
+
+            app.MapDelete("/v1/user", async ([FromBody] DeleteUserInput requestInput, AppDbContext context, IHasherService hasherService) =>
+            {
+                var userToDelete = await context.Users.SingleOrDefaultAsync(u => u.Id == requestInput.Id);
+
+                if (userToDelete is null || !hasherService.VerifyPasswordHash(userToDelete.Password, requestInput.ConfirmPassword))
+                    return Results.BadRequest("Credenciais incorretas");
+
+                context.Users.Remove(userToDelete);
+
+                await context.SaveChangesAsync();
+
+                return Results.Ok(new { message = "Usuário deletado com sucesso.", data = userToDelete });
+            });
+        }
+    }
+}
