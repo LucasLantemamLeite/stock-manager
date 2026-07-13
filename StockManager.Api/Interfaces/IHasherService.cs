868977b7f0fbe020fea8cf55e6fbc894cb58b567
LucasLantemamLeite <Lucaslantemamleite2005@gmail.com>
2026-07-12 22:23:54 -0300

    feat: cria contrato para serviço de hash para isolar o método de hash de senha

---
+++ StockManager.Api/Interfaces/IHasherService.cs
@@ -0,0 +1,8 @@
+﻿namespace StockManager.Api.Interfaces;
+
+public interface IHasherService
+{
+    string GeneratePasswordHash(string passwordTarget);
+
+    bool VerifyPasswordHash(string passwordHash, string passwordTarget);
+}
