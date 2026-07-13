7139ff075edf05261a2f98c35373961005b310b5
LucasLantemamLeite <Lucaslantemamleite2005@gmail.com>
2026-07-12 22:24:34 -0300

    feat: cria a implementação do serviço de hash utilizando BCrypt

---
+++ StockManager.Api/Services/BCryptHashService.cs
@@ -0,0 +1,13 @@
+﻿using StockManager.Api.Interfaces;
+using BC = BCrypt.Net.BCrypt;
+
+namespace StockManager.Api.Services;
+
+public sealed class BCryptHashService : IHasherService
+{
+    public string GeneratePasswordHash(string passwordTarget)
+        => BC.HashPassword(passwordTarget);
+
+    public bool VerifyPasswordHash(string passwordHash, string passwordTarget)
+        => BC.Verify(passwordTarget, passwordHash);
+}
