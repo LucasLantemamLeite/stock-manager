f300a51171afcfdc62dd1c47092da6a399250c8d
LucasLantemamLeite <Lucaslantemamleite2005@gmail.com>
2026-07-14 16:00:19 -0300

    feat: adiciona a validação dos dados recebidos pelo cliente, evitando erros no banco de dados

--- StockManager.Api/Requests/Inputs/CreateUserInput.cs
+++ StockManager.Api/Requests/Inputs/CreateUserInput.cs
@@ -1,9 +1,28 @@
-﻿namespace StockManager.Api.Requests.Inputs;
+﻿using StockManager.Api.Enums;
+using System.ComponentModel.DataAnnotations;
 
-public sealed record CreateUserInput(
-    string Name,
-    string Email,
-    string Phone,
-    string Password,
-    sbyte Role
-);
+namespace StockManager.Api.Requests.Inputs;
+
+public sealed record CreateUserInput
+{
+    [Required(ErrorMessage = "O campo 'nome' é obrigatório.")]
+    [MaxLength(50, ErrorMessage = "O campo 'nome' não deve ser superior a 50 caracteres.")]
+    public required string Name { get; init; }
+
+    [Required(ErrorMessage = "O campo 'e-mail' é obrigatório.")]
+    [MaxLength(255, ErrorMessage = "O campo 'e-mail' não deve ser superior a 255 caracteres.")]
+    public required string Email { get; init; }
+
+    [Required(ErrorMessage = "O campo 'número de telefone' é obrigatório.")]
+    [MaxLength(20, ErrorMessage = "O campo 'número de telefone' não deve ser superior a 20 caracteres.")]
+    public required string Phone { get; init; }
+
+    [Required(ErrorMessage = "O campo 'senha' é obrigatório.")]
+    [MinLength(8, ErrorMessage = "O campo 'senha' deve ser superior a 8 caracteres.")]
+    [MaxLength(30, ErrorMessage = "O campo 'senha' não deve ser superior a 30 caracteres.")]
+    public required string Password { get; init; }
+
+    [Required(ErrorMessage = "O campo 'nível' é obrigatório.")]
+    [Range(1, 2, ErrorMessage = "O campo 'nível' deve estar entre os valores 1 e 2.")]
+    public required Role Role { get; init; }
+};
