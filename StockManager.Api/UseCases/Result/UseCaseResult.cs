d3cac1a62498aeac05d3173454e4770422177d4b
LucasLantemamLeite <Lucaslantemamleite2005@gmail.com>
2026-07-15 19:17:44 -0300

    refactor: remove as propriedades explicitas desnecessárias e deixa o record criar automaticamente a partir do construtor

--- StockManager.Api/UseCases/Result/UseCaseResult.cs
+++ StockManager.Api/UseCases/Result/UseCaseResult.cs
@@ -7,13 +7,10 @@ public sealed record UseCaseResult<TData>(HttpStatusCode HttpStatusCode, string 
 {
     private HttpStatusCode HttpStatusCode { get; } = HttpStatusCode;
     public int IntStatusCode => (int)HttpStatusCode;
-    public string Message { get; } = Message;
-    public TData? Data { get; } = Data;
 }
 
 public sealed record UseCaseResult(HttpStatusCode HttpStatusCode, string Message)
 {
     private HttpStatusCode HttpStatusCode { get; } = HttpStatusCode;
     public int IntStatusCode => (int)HttpStatusCode;
-    public string Message { get; } = Message;
 }
