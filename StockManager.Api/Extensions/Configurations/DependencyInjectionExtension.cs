0eee8ccb34c21613a54d41edd48924bde2e6db84
LucasLantemamLeite <Lucaslantemamleite2005@gmail.com>
2026-07-15 18:45:51 -0300

    refactor: separa a configuração de injeção de dependência em um método de extensão separado

---
+++ StockManager.Api/Extensions/Configurations/DependencyInjectionExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockManager.Api.Data.Context;
+using StockManager.Api.Services.Implementation;
+using StockManager.Api.Services.Interfaces;
+using StockManager.Api.UseCases.Users;
+
+namespace StockManager.Api.Extensions.Configurations;
+
+public static class DependencyInjectionExtension
+{
+    public static void ResolveDependency(this WebApplicationBuilder builder, string secretKey)
+    {
+        var dbConnectionString = builder.Configuration.GetConnectionString("Default")
+            ?? throw new InvalidOperationException("ConnectionString do banco de dados não encontrada no appsettings.");
+
+        builder.Services.AddDbContext<AppDbContext>(c => c.UseSqlServer(dbConnectionString));
+
+        builder.Services.AddSingleton<ITokenService>(new JwtTokenService(secretKey));
+        builder.Services.AddSingleton<IHasherService, BCryptHashService>();
+
+        builder.Services.AddTransient<CreateUserUseCase>();
+        builder.Services.AddTransient<LoginUserUseCase>();
+        builder.Services.AddTransient<UpdateUserUseCase>();
+        builder.Services.AddTransient<DeleteUserUseCase>();
+    }
+}
--- StockManager.Api/Program.cs
+++ StockManager.Api/Program.cs
@@ -1,33 +1,18 @@
-using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
-using StockManager.Api.Data.Context;
 using StockManager.Api.Extensions.Configurations;
 using StockManager.Api.Middlewares;
-using StockManager.Api.Services.Implementation;
-using StockManager.Api.Services.Interfaces;
-using StockManager.Api.UseCases.Users;
 
 var builder = WebApplication.CreateBuilder(args);
-
-var dbConnectionString = builder.Configuration.GetConnectionString("Default");
-
-builder.Services.AddDbContext<AppDbContext>(c => c.UseSqlServer(dbConnectionString));
-
-var secretKey = builder.Configuration.GetValue<string>("SecretKey")
-    ?? throw new InvalidOperationException("SecretKey não encontrada no appsettings!");
-
-builder.Services.ConfigureJwtAuthentication(secretKey);
 
 builder.Services.AddControllers()
     .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);
 
-builder.Services.AddSingleton<ITokenService>(new JwtTokenService(secretKey));
-builder.Services.AddSingleton<IHasherService, BCryptHashService>();
+var secretKey = builder.Configuration.GetValue<string>("SecretKey")
+    ?? throw new InvalidOperationException("SecretKey não encontrada no appsettings.");
 
-builder.Services.AddTransient<CreateUserUseCase>();
-builder.Services.AddTransient<LoginUserUseCase>();
-builder.Services.AddTransient<UpdateUserUseCase>();
-builder.Services.AddTransient<DeleteUserUseCase>();
+builder.Services.ConfigureJwtAuthentication(secretKey);
+
+builder.ResolveDependency(secretKey);
 
 builder.Services.AddOpenApi();
 
