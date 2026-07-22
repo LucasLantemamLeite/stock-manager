3370b12aaf594dab8e06e77c462ff5c89280504c
Lucas Lantemam Leite <lucaslantemamleite2005@gmail.com>
2026-07-22 16:09:44 -0300

    feat: adiciona retorno 500 de erro interno em todas as rotas

--- StockManager.Api/Extensions/OpenApi/OperationTransformerExtension.cs
+++ StockManager.Api/Extensions/OpenApi/OperationTransformerExtension.cs
@@ -1,4 +1,6 @@
-﻿namespace StockManager.Api.Extensions.OpenApi;
+﻿using Microsoft.OpenApi;
+
+namespace StockManager.Api.Extensions.OpenApi;
 
 public static class OperationTransformerExtension
 {
@@ -7,7 +9,27 @@ public static class OperationTransformerExtension
         services.AddOpenApi(options =>
             options.AddOperationTransformer((operation, context, cancellationToken) =>
             {
-
+                operation?.Responses?.TryAdd("500", new OpenApiResponse()
+                {
+                    Description = "InternalServerError",
+                    Content = new Dictionary<string, OpenApiMediaType>()
+                    {
+                        ["application/json"] = new OpenApiMediaType()
+                        {
+                            Schema = new OpenApiSchema()
+                            {
+                                Type = JsonSchemaType.Object,
+                                Properties = new Dictionary<string, IOpenApiSchema>()
+                                {
+                                    ["message"] = new OpenApiSchema()
+                                    {
+                                        Type = JsonSchemaType.String,
+                                    },
+                                }
+                            }
+                        }
+                    }
+                });
 
                 return Task.CompletedTask;
             })
