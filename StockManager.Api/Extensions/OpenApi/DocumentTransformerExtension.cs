26c7c8b0d2619f3fee9d59cb65b8eeee76e4b20a
Lucas Lantemam Leite <lucaslantemamleite2005@gmail.com>
2026-07-24 17:29:30 -0300

    feat: adiciona melhorias na geração e organização da documentação do servidor

--- StockManager.Api/Extensions/OpenApi/DocumentTransformerExtension.cs
+++ StockManager.Api/Extensions/OpenApi/DocumentTransformerExtension.cs
@@ -9,22 +9,47 @@ public static class DocumentTransformerExtension
         services.AddOpenApi(options =>
             options.AddDocumentTransformer((document, context, cancellationToken) =>
             {
+                const string infoTitle = "Stock Manager | v1";
+                const string infoDescription = "Stock Manager é um sistema de gerênciamento de empresas, usuários, produtos e armazéns.";
+                const string infoVersion = "1.0.0";
+              
                 document.Info = new OpenApiInfo()
                 {
-                    Title = "StockManager",
-                    Version = "v1",
-                    Description = "Stock Manager é um sistema de gerênciamento de empresas, usuários, produtos e armazéns"
+                    Title = infoTitle,
+                    Description = infoDescription,
+                    Version = infoVersion,
+                };
+                
+                const string serverUrl = "{protocol}://localhost:5034/v1";
+                const string serverDescription = "Rota base rodando na máquina local.";
+                var serverVariables = new Dictionary<string, OpenApiServerVariable>()
+                {
+                    ["protocol"] = new()
+                    {
+                        Enum = ["http", "https"],
+                        Default = "https"
+                    }
+                };
+                
+                document.Servers = new List<OpenApiServer>()
+                {
+                    new()
+                    {
+                        Url = serverUrl,
+                        Description = serverDescription,
+                        Variables = serverVariables
+                    },
                 };
 
-                document.Tags = new HashSet<OpenApiTag>
+                document.Tags = new HashSet<OpenApiTag>()
                 {
                     new()
                     {
                         Name = "Users",
-                        Description = "Rotas responsáveis pelas ações dos usuários"
+                        Description = "Responsável pelas operações referente ao usuário."
                     }
                 };
-
+                
                 return Task.CompletedTask;
             })
         );
