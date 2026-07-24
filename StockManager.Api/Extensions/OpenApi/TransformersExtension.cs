2779daa8a7f271bd414d52f9d78e29a795421d0c
Lucas Lantemam Leite <lucaslantemamleite2005@gmail.com>
2026-07-24 17:39:19 -0300

    refactor: junta os métodos de extensão do OpenApi em um arquivo único

---
+++ StockManager.Api/Extensions/OpenApi/TransformersExtension.cs
@@ -0,0 +1,71 @@
+using Microsoft.OpenApi;
+
+namespace StockManager.Api.Extensions.OpenApi;
+
+public static class TransformersExtension
+{
+    extension(IServiceCollection services)
+    {
+        public void AddDocumentTransformerOpenApi()
+        {
+            services.AddOpenApi(options =>
+                options.AddDocumentTransformer((document, context, cancellationToken) =>
+                {
+                    const string infoTitle = "Stock Manager | v1";
+                    const string infoDescription = "Stock Manager é um sistema de gerênciamento de empresas, usuários, produtos e armazéns.";
+                    const string infoVersion = "1.0.0";
+              
+                    document.Info = new OpenApiInfo()
+                    {
+                        Title = infoTitle,
+                        Description = infoDescription,
+                        Version = infoVersion,
+                    };
+                
+                    const string serverUrl = "{protocol}://localhost:5034/v1";
+                    const string serverDescription = "Rota base rodando na máquina local.";
+                    var serverVariables = new Dictionary<string, OpenApiServerVariable>()
+                    {
+                        ["protocol"] = new()
+                        {
+                            Enum = ["http", "https"],
+                            Default = "https"
+                        }
+                    };
+                
+                    document.Servers = new List<OpenApiServer>()
+                    {
+                        new()
+                        {
+                            Url = serverUrl,
+                            Description = serverDescription,
+                            Variables = serverVariables
+                        },
+                    };
+            
+                    document.Tags = new HashSet<OpenApiTag>()
+                    {
+                        new()
+                        {
+                            Name = "Users",
+                            Description = "Responsável pelas operações referente ao usuário."
+                        }
+                    };
+                
+                    return Task.CompletedTask;
+                })
+            );
+        }
+
+        public void AddOperationTransformerOpenApi()
+        {
+            services.AddOpenApi(options =>
+                options.AddOperationTransformer((operation, context, cancellationToken) =>
+                {
+
+                    return Task.CompletedTask;
+                })
+            );
+        }
+    }
+}
