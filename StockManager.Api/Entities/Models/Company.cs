514c74a7ff3c934c1ce09912197dc13f3739dce1
Lucas Lantemam Leite <lucaslantemamleite2005@gmail.com>
2026-08-07 21:19:23 -0300

    feat: cria a classe de empresa

---
+++ StockManager.Api/Entities/Models/Company.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using StockManager.Api.Entities.Base;
+
+namespace StockManager.Api.Entities.Models;
+
+public sealed class Company : Entity
+{
+    public Company(string name, string cnpj, Guid ownerId)
+    {
+        Name = name;
+        Cnpj = cnpj;
+        OwnerId = ownerId;
+    }
+
+    public Company(Guid id, string name, string cnpj, Guid ownerId, DateTime createdAt, DateTime updatedAt) : base(id)
+    {
+        Name = name;
+        Cnpj = cnpj;
+        OwnerId = ownerId;
+        CreatedAt = createdAt;
+        UpdatedAt = updatedAt;
+    }
+
+    public string Name { get; private set; }
+    public string Cnpj { get;  }
+    public ICollection<User> Users { get; private set; } = [];
+    public Guid OwnerId { get; private set; }
+    public DateTime CreatedAt { get; } = DateTime.UtcNow;
+    public DateTime UpdatedAt { get; private set; } = DateTime.UtcNow;
+}
