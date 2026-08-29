93fa4e7116f3a57a945124ecf055018c62cd2da8
Lucas Lantemam Leite <lucaslantemamleite2005@gmail.com>
2026-08-29 16:53:05 -0300

    feat: adiciona a rota no controller para criar novas empresas

---
+++ StockManager.Api/Controllers/CompanyController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using StockManager.Api.Contracts.Companies.Inputs;
+using StockManager.Api.UseCases.Companies;
+
+namespace StockManager.Api.Controllers;
+
+[ApiController]
+[Tags("Companies")]
+[Route("v1/company")]
+public sealed class CompanyController : ControllerBase
+{
+    [AllowAnonymous]
+    [HttpPost]
+    public async Task<IActionResult> CreateCompanyAsync([FromServices] CreateCompanyUseCase createCompanyUseCase, [FromBody] CreateCompanyInput createCompanyInput)
+    {
+        var useCaseResult = await createCompanyUseCase.ExecuteAsync(createCompanyInput);
+
+        return StatusCode(useCaseResult.IntStatusCode, useCaseResult);
+    }
+}
