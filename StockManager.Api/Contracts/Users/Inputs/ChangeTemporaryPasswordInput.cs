1c5c4472c8487bcf52374d6609dc6dc619f7085f
Lucas Lantemam Leite <lucaslantemamleite2005@gmail.com>
2026-08-31 22:31:00 -0300

    feat: cria a entrada do body da requisição para enviar os dados necessários para a alteração da senha temporária

---
+++ StockManager.Api/Contracts/Users/Inputs/ChangeTemporaryPasswordInput.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace StockManager.Api.Contracts.Users.Inputs;
+
+public sealed record ChangeTemporaryPasswordInput : ConfirmPasswordInput
+{
+    [Required(ErrorMessage = "É obrigatório.")]
+    [MaxLength(255, ErrorMessage = "Não deve ser superior a 255 caracteres.")]
+    [EmailAddress(ErrorMessage = "Formato inválido.")]
+    [Description("Email da conta do usuário.")]
+    public required string Email { get; init; }
+    
+    [Required(ErrorMessage = "É obrigatório.")]
+    [Description("Nova senha da conta do usuário.")]
+    public required string NewPassword { get; init; }
+};
