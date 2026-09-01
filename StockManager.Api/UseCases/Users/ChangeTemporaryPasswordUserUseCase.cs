e16db4aef275a95929f5c9e68ad527cafc70972a
Lucas Lantemam Leite <lucaslantemamleite2005@gmail.com>
2026-08-31 22:30:05 -0300

    feat: cria o caso de uso para alterar a senha temporária gerada pelo sistema para uma senha do qual o usuário escolheu

---
+++ StockManager.Api/UseCases/Users/ChangeTemporaryPasswordUserUseCase.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using Microsoft.EntityFrameworkCore;
+using StockManager.Api.Contracts.Users.Inputs;
+using StockManager.Api.Data.Context;
+using StockManager.Api.Services.Interfaces;
+using StockManager.Api.UseCases.Result;
+
+namespace StockManager.Api.UseCases.Users;
+
+public sealed class ChangeTemporaryPasswordUserUseCase(
+    AppDbContext appDbContext,
+    IHasherService hasherService,
+    ITokenService tokenServiceo)
+{
+    public async Task<UseCaseResult<string>> ExecuteAsync(ChangeTemporaryPasswordInput changeTemporaryPasswordInput)
+    {
+        var userToChangePassword =
+            await appDbContext.Users.SingleOrDefaultAsync(u => u.Email.Equals(changeTemporaryPasswordInput.Email));
+
+        if (userToChangePassword is null || !hasherService.VerifyPasswordHash(userToChangePassword.Password,
+                changeTemporaryPasswordInput.ConfirmPassword))
+            return new UseCaseResult<string>(
+                HttpStatusCode.Unauthorized,
+                "Credenciais incorretas."
+            );
+
+        var newPasswordHash = hasherService.GeneratePasswordHash(changeTemporaryPasswordInput.NewPassword);
+        
+        userToChangePassword.SetPassword(newPasswordHash);
+        userToChangePassword.SetPasswordMustBeChangedToFalse();
+
+        await appDbContext.SaveChangesAsync();
+
+        var userAuthToken = tokenServiceo.GenerateAuthToken(userToChangePassword);
+
+        return new UseCaseResult<string>(
+            HttpStatusCode.OK,
+            "",
+            userAuthToken
+        );
+    }
+}
